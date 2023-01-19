823d0e4a4fd96fd044fd8c60a65c3ae75be67552
khdrali <khaidarali48@gmail.com>
2023-01-20 01:18:24 +0700

    [ADD] Sign-Up

---
+++ src/pages/Sign-up.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../styles/Sign-up.css'
+
+function SignUp(){
+    return(
+        <div>
+             <div class="row">
+        {/* <!--Left--> */}
+        <div class="col-6 background-login">
+          <img src="image/tickitz 1.png" alt='cover'/>
+          <p>wait, watch, wow!</p>
+        </div>
+        {/* <!--Right--> */}
+      <div class="col-6 form-sign-up">
+        <div>
+        <h2>Sign Up</h2>
+        <p>Fill your additional details</p>
+        <div class="mb-3 form-width">
+          <label for="name-input" class="form-label form-control-lg">First Name</label>
+          <input type="name" class="form-control form-tabel" id="name-input" placeholder="Write Your First Name"/>
+        </div>
+        <div class="mb-3 form-width">
+          <label for="name-input" class="form-label form-control-lg">Last Name</label>
+          <input type="name" class="form-control form-tabel" id="name-input" placeholder="Write Your Last Name"/>
+        </div>
+        <div class="mb-3 form-width">
+          <label for="phone-input" class="form-label form-control-lg">Phone Number</label>
+          <input type="number" class="form-control form-tabel" id="phone-input" placeholder="Write Your Phone Number"/>
+        </div>
+        <div class="mb-3 form-width"> 
+          <label for="email-input" class="form-label form-control-lg">Email</label>
+          <input type="email" class="form-control form-tabel" id="email-input" placeholder="Write Your Email"/>
+        </div>
+        <div class="mb-3 form-width">
+            <label for="password-input" class="form-label form-control-lg">Password</label>
+            <input type="password" class="form-control form-tabel" id="password-input" placeholder="Write Your Password"/>
+        </div>
+        <div class="d-grid gap-2 but">
+            <button type="button" class="btn btn-warning color">
+              <Link class="text" to="">Sign Up</Link>
+              </button>
+        </div>
+        <p class="accc">
+            Already have account?
+            <Link class="sign-in" to="">Sign In</Link>
+          </p>
+    </div>
+    </div>
+    </div>
+        </div>
+    )
+}
+
+export default SignUp
