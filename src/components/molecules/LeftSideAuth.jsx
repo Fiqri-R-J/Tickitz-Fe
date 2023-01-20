750ebff47ade2364a8335143eb3059cf4fc31460
Alviando <restualviando28@gmail.com>
2023-01-20 14:55:02 +0700

    [UPDATE] Modularisasi Component Login Page

---
+++ src/components/molecules/LeftSideAuth.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function LeftSideAuth() {
+  return (
+    <div id="left-side-auth">
+      <div className="left-side-auth d-flex">
+        <div className="overlay-background">
+          <div className="left-side-group text-center">
+            <img
+              src="https://asset.tix.id/wp-content/uploads/2021/10/TIXID_logo_inverse-300x82.png"
+              alt=""
+              width={"400px"}
+            />
+            <p className="mt-4">wait, watch, wow!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LeftSideAuth;
